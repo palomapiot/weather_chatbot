6bde6991f827c99bc652bc16653d1b6deace6516
Paloma Piot <paloma.piot@viesure.io>
2022-04-21 22:20:52 +0200

    Rasa weather chatbot - Xornadas Anita Borg

---
+++ actions/actions.py
@@ -0,0 +1,39 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+class ActionWeather(Action):
+
+    def name(self) -> Text:
+        return "action_weather_api"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot("city")
+        temp = int(weather(city)['temp'] - 273)
+
+        dispatcher.utter_message(response="utter_weather", temp=temp)
+
+        return []
+
+def weather(city):
+    api_adress = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+    url = api_adress + city
+    json_data = requests.get(url).json()
+
+    format_weather = json_data['main']
+
+    return format_weather
